4098da1c043425cd64b0355e9db8ce633e8b0aa3
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-07-30 23:10:00 +0500

    create models for request and response

---
+++ src/main/java/com/evgeniyfedorchenko/gptbot/yandex/models/GptAnswer.java
@@ -0,0 +1,39 @@
+package com.evgeniyfedorchenko.gptbot.yandex.models;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record GptAnswer(@NotNull GptAnswer.Result result) {
+
+    public record Result(List<Alternative> alternatives, Usage usage, String modelVersion) { }
+
+    public record Alternative(Message message, String status) { }
+
+    public record Usage(int inputTextTokens, int completionTokens, int totalTokens) { }
+
+}
+
+/*
+Response:
+
+{
+  "result": {
+    "alternatives": [
+      {
+        "message": {
+          "role": "assistant",
+          "text": "Ответ модели здесь"
+        },
+        "status": "ALTERNATIVE_STATUS_FINAL"
+      }
+    ],
+    "usage": {
+      "inputTextTokens": "29",
+      "completionTokens": "12",
+      "totalTokens": "41"
+    },
+    "modelVersion": "18.01.2024"
+  }
+}
+*/
---
+++ src/main/java/com/evgeniyfedorchenko/gptbot/yandex/models/GptRequestBody.java
@@ -0,0 +1,65 @@
+package com.evgeniyfedorchenko.gptbot.yandex.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * Класс представляет собой тело запроса, который направляется на обработку модели Яндекса
+ */
+@Builder
+@Getter
+public class GptRequestBody {
+
+    /**
+     * Внутренний адрес модели Яндекса
+     * @example {@code "gpt://<your-yandex-folder-id>/yandexgpt-lite}
+     */
+    private final String modelUri;
+
+    /**
+     * Настройки ответа: режим потока, температура, ограничение по токенам
+     */
+    private final CompletionOptions completionOptions;
+
+    /**
+     * История сообщений,"контекст"
+     */
+    private final List<Message> messages;
+
+    /**
+     * Технические настройки желаемого ответа
+     *
+     * @param stream      Режим потока - передавать ответ только когда он будет полностью готов или по частям
+     * @param temperature Мера непредсказуемости/креативности. Число от [0,1]
+     * @param maxTokens   Лимит токенов на ответ, максимум 2000 токенов
+     */
+    public record CompletionOptions(boolean stream, double temperature, int maxTokens) { }
+
+}
+
+/*
+Request:
+
+curl --request POST
+ -H "Content-Type: application/json"
+ -H "Authorization: Bearer {your_iam_token}"
+ -H "x-folder-id: {your_folder_id}"
+ -d '{
+  "modelUri": "gpt://{your_folder_id}/yandexgpt-lite",
+  "completionOptions": {
+    "stream": false,
+    "temperature": 0.6,
+    "maxTokens": "2000"
+  },
+  "messages": [
+  *Вся история чата. Новые - снизу*
+    {
+      "role": "user",
+      "text": "Привет, это тестовый запрос. Отзовись, если ты слышишь меня"
+    }
+  ]
+}'   "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+
+*/
