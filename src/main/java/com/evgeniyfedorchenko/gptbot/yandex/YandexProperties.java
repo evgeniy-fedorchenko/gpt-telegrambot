9443f788b9b48881dc299f12ccb14268ad0cffee
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-08-02 00:36:09 +0500

    refactor and validation yandex properties

--- src/main/java/com/evgeniyfedorchenko/gptbot/yandex/YandexProperties.java
+++ src/main/java/com/evgeniyfedorchenko/gptbot/yandex/YandexProperties.java
@@ -1,29 +1,54 @@
 package com.evgeniyfedorchenko.gptbot.yandex;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Setter
 @Getter
 @Component
+@Validated
 @ConfigurationProperties(
         prefix = YandexProperties.CONFIGURATION_PREFIX,
-        ignoreUnknownFields = false
+        ignoreUnknownFields = false,
+        ignoreInvalidFields = false
 )
 public class YandexProperties {
 
     static final String CONFIGURATION_PREFIX = "yandex";
 
-    private String baseUrl;
+    @NotBlank
+    private String chatbotBaseUrl;
 
+    @NotBlank
+    private String artModelBaseUrl;
+
+    @NotBlank
+    private String chatbotUriPattern;
+
+    @NotBlank
+    private String artModelUriPattern;
+
+    @NotEmpty
+    @URL(protocol = "https")
+    private String artModelCompleteUrlPattern;
+
+    @NotEmpty
+    @URL(protocol = "https")
+    private String iamTokenUpdaterUrl;
+
+    @NotEmpty
+    @Pattern(regexp = "^[a-z0-9]{15,25}$")
     private String folderId;
 
+    @NotEmpty
+    @Pattern(regexp = "^y[0-3]_[a-zA-Z0-9_-]{58}$")
     private String oauthToken;
-
-    private String modelUriPattern;
-
-    private String iamTokenUpdaterUrl;
 
 }
