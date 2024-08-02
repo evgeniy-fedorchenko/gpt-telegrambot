ba77605f76c4338ebf2a99eb65fbed5d75c51103
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-08-03 02:33:02 +0500

    reconfigure webclient as inject httpClient and add timeouts

--- src/main/java/com/evgeniyfedorchenko/gptbot/configuration/WebClientConfiguration.java
+++ src/main/java/com/evgeniyfedorchenko/gptbot/configuration/WebClientConfiguration.java
@@ -3,6 +3,10 @@ package com.evgeniyfedorchenko.gptbot.configuration;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -12,28 +16,42 @@ import reactor.netty.http.client.HttpClient;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+@Getter
+@Setter
 @Configuration
+@ConfigurationProperties(
+        prefix = WebClientConfiguration.CONFIGURATION_PREFIX,
+        ignoreUnknownFields = false,
+        ignoreInvalidFields = false
+)
 public class WebClientConfiguration {
 
-    private static final int CONNECT_TIMEOUT_MILLIS = 2_000;
-    private static final int RESPONSE_TIMEOUT_MILLIS = 10_000;
-    private static final int READ_TIMEOUT_MILLIS = 10_000;
-    private static final int WRITE_TIMEOUT_MILLIS = 5_000;
+    static final String CONFIGURATION_PREFIX = "http-client";
 
-    private static final int RESPONSE_MAX_BYTES_COUNT = 10_485_760;
+    @Positive
+    private int connectTimeout;
+    @Positive
+    private int responseTimeout;
+    @Positive
+    private int readTimeout;
+    @Positive
+    private int writeTimeout;
+    @Positive
+    private int responseMaxSize;
+
 
     @Bean
     public WebClient webClient() {
 
         HttpClient nettyHttpClient = HttpClient.create()
 
-                .httpResponseDecoder(spec -> spec.maxHeaderSize(RESPONSE_MAX_BYTES_COUNT))
+                .httpResponseDecoder(spec -> spec.maxHeaderSize(responseMaxSize))
 
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECT_TIMEOUT_MILLIS)
-                .responseTimeout(Duration.ofMillis(RESPONSE_TIMEOUT_MILLIS))
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeout)
+                .responseTimeout(Duration.ofMillis(responseTimeout))
                 .doOnConnected(connection ->
-                        connection.addHandlerLast(new ReadTimeoutHandler(READ_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS))
-                                .addHandlerLast(new WriteTimeoutHandler(WRITE_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS))
+                        connection.addHandlerLast(new ReadTimeoutHandler(readTimeout, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(writeTimeout, TimeUnit.MILLISECONDS))
                 );
 
 
