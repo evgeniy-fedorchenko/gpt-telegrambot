dc1750b4e61332df433363bf95618eaa8495fa1d
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-08-11 02:27:37 +0500

    add overloaded default methods in redis service interfaces for choose type of 'userChatId' parameter

--- src/main/java/com/evgeniyfedorchenko/gptbot/data/HistoryRedisService.java
+++ src/main/java/com/evgeniyfedorchenko/gptbot/data/HistoryRedisService.java
@@ -7,7 +7,13 @@ import java.util.List;
 public interface HistoryRedisService {
 
     List<GptMessageUnit> getHistory(String userChatId);
+    default List<GptMessageUnit> getHistory(Long userChatId) {
+        return this.getHistory(String.valueOf(userChatId));
+    }
 
     void addMessage(String userChatId, GptMessageUnit gptMessageUnit);
+    default void addMessage(Long userChatId, GptMessageUnit gptMessageUnit) {
+        this.addMessage(String.valueOf(userChatId), gptMessageUnit);
+    }
 
 }
--- src/main/java/com/evgeniyfedorchenko/gptbot/data/UserModeRedisService.java
+++ src/main/java/com/evgeniyfedorchenko/gptbot/data/UserModeRedisService.java
@@ -5,6 +5,13 @@ import com.evgeniyfedorchenko.gptbot.telegram.Mode;
 public interface UserModeRedisService {
 
     Mode getMode(String userChatId);
+    default Mode getMode(Long userChatId) {
+        return this.getMode(String.valueOf(userChatId));
+    }
 
     void setMode(String userChatId, Mode mode);
+
+    default void setMode(Long userChatId, Mode mode) {
+        this.setMode(String.valueOf(userChatId), mode);
+    }
 }
