503544b760ea13076ccc274723bbf52083067f20
evgeniy-fedorchenko <jecky432@gmail.com>
2024-10-22 22:21:06 +0500

    scheduled-exec-fix: few edits in Helper.java

--- src/main/java/com/efedorchenko/gptbot/utils/Helper.java
+++ src/main/java/com/efedorchenko/gptbot/utils/Helper.java
@@ -3,24 +3,16 @@ package com.efedorchenko.gptbot.utils;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.Update;
 import reactor.util.annotation.Nullable;
 
 import java.util.Optional;
 
-@Slf4j
 @Component
-@RequiredArgsConstructor
 public class Helper {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper(); {{
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    }}
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
     public static String write(@Nullable Object object) {
         if (object instanceof String string) {
@@ -37,7 +29,6 @@ public class Helper {
         try {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException ignore) {
-            log.warn("WARNNN   " + ignore);
             return object == null ? null : object.toString();
         }
     }
