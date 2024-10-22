c092074b035fb4340012d788f1f6cf9a2b4890b9
evgeniy-fedorchenko <jecky432@gmail.com>
2024-10-22 20:42:34 +0500

    scheduled-exec-fix: set nullest annotation for more clarity of the method

--- src/main/java/com/efedorchenko/gptbot/utils/Helper.java
+++ src/main/java/com/efedorchenko/gptbot/utils/Helper.java
@@ -2,19 +2,21 @@ package com.efedorchenko.gptbot.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import reactor.util.annotation.Nullable;
 
 @Component
 public class Helper {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public static String write(Update update) {
+    public static @NotNull String write(@Nullable Update update) {
         try {
             return OBJECT_MAPPER.writeValueAsString(update);
         } catch (JsonProcessingException ignore) {
-            return "<cannot serialize>. Use 'toString()': " + update.toString();
+            return "<cannot serialize>. Use 'toString()': " + (update == null ? null : update.toString());
         }
     }
 
